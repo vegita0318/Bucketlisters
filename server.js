7cfdb06a35e9fc629289404b1b96e2e9ea2e8726
Larry Fuller <lfullerj@gmail.com>
2019-02-05 20:13:47 -0500

    Trying to fix git issues

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
-// require('babel-register');  // Having this we can use 'import' instead of require in nodeapp.
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
 
-require('./server.babel');
+var tasks = require("./routes/tasks");
+
+const cors = require("cors");
+
+var port = 5000;  
+
+var app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials:true
+    })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("views engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+app.use(express.static(path.join(__dirname, "client")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use("/api", tasks);
+
+app.listen(port, function() {
+    console.log("Server started on port " + port);
+})
